4edf04e23a35665e76d4ae052db664aff8c8a3e2
tgelmi-candusso <tiz.gelmi@gmail.com>
2024-04-03 10:14:55 -0400

    outline

---
+++ vectors.R
@@ -0,0 +1,11 @@
+# 1) download a shapefile 
+## 1.a) use a shapefile from protected areas or e.g. roads
+## 1.b) use OSM to download urban parks or cemeteries
+# 2) inspect the crs, reproject, make them match
+# 3) inspect contents columns, it looks as a dataframe in fact we can convert it into a dataframe
+# 3) Plot it using plot or plet
+# 4) estimate distance to closest polygon from point
+# 4) generate buffer polygons from the points and estimate surface area from polygons
+# 5) put info on a table and extract csv
+# 6) extract certain polygons from vector
+# 7) save vector into a shapefile your computer
